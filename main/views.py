98991f05f8a53a8c1ceb8e9d386799fae26f1541
Jacob Rogers <intervicker@gmail.com>
2014-08-19 23:08:14 -0500

    try sending email with mandrill

--- main/views.py
+++ main/views.py
@@ -86,6 +86,38 @@ def signup(request):
     else:
         return HttpResponse(status=400)
 
+def send_email(request):
+    import os
+    import smtplib
+
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+
+    msg = MIMEMultipart('alternative')
+
+    msg['Subject'] = "Hello from Mandrill, Python style!"
+    msg['From']    = "John Doe <john@doe.com>" # Your from name and email address
+    msg['To']      = "intervicker@gmail.com"
+
+    text = "Mandrill speaks plaintext"
+    part1 = MIMEText(text, 'plain')
+
+    html = "<em>Mandrill speaks <strong>HTML</strong></em>"
+    part2 = MIMEText(html, 'html')
+
+    username = os.environ['MANDRILL_USERNAME']
+    password = os.environ['MANDRILL_PASSWORD']
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    s = smtplib.SMTP('smtp.mandrillapp.com', 587)
+
+    s.login(username, password)
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    s.quit()
+
 @ensure_csrf_cookie
 def save_groups(request):
     if request.method == 'POST':
