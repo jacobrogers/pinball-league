79ec441c788ffa6914ebc97fb4c81a2423bcf172
Jacob Rogers <intervicker@gmail.com>
2014-09-06 01:10:35 -0500

    delete old angular stuff

--- assets/js/src/playersController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('controllers')
-.controller('PlayersCtrl', ['$scope','$http', function($scope,$http) {
-	$http.get('/api/players').success(function(data) {
-		$scope.players = data;
-	});
-}]);
--- assets/js/src/tablesController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('controllers')
-.controller('TablesCtrl', ['$scope','$http', function($scope,$http) {
-	$http.get('/api/tables').success(function(data) {
-		$scope.tables = data;
-	});
-}]);
