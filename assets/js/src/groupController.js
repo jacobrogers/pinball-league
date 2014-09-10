33752d54e4bce5409a6944cfb01d97589197a28d
Jacob Rogers <intervicker@gmail.com>
2014-09-10 00:24:11 -0500

    add red color to invalid score

--- assets/js/src/groupController.js
+++ assets/js/src/groupController.js
@@ -13,18 +13,29 @@ angular.module('controllers')
 
 	$scope.saveTable = function(table) {
 		var gamesToSave = $scope.group.games.filter(function(game) { return game.table.name === table.name });
-		$http.post('/api/saveGames', {games: gamesToSave, week: $scope.week, group: $scope.groupNumber})
-			.success(function(data) {
-				for (var i in data) {
-					var gamePoints = data[i];
-					for (var j in gamesToSave) {
-						var gameToSave = gamesToSave[j];
-						if (gameToSave.id == gamePoints.id) {
-							gameToSave.league_points = gamePoints.league_points;
-							gameToSave.bonus_points = gamePoints.bonus_points;
+		var scoresAreValid = true;
+		_.each(gamesToSave, function(game) {
+			if (!_.isNumber(game.score)) {
+				scoresAreValid = false;
+				game.status = 'invalidScore';
+			}
+		});
+
+		if (scoresAreValid) {
+			$http.post('/api/saveGames', {games: gamesToSave, week: $scope.week, group: $scope.groupNumber})
+				.success(function(data) {
+					for (var i in data) {
+						var gamePoints = data[i];
+						for (var j in gamesToSave) {
+							var gameToSave = gamesToSave[j];
+							if (gameToSave.id == gamePoints.id) {
+								gameToSave.league_points = gamePoints.league_points;
+								gameToSave.bonus_points = gamePoints.bonus_points;
+								gameToSave.status = 'saved';
+							}
 						}
 					}
-				}
-			});
+				});
+		}
 	};
 }]);
