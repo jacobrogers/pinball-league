ae717931fc411700e8b52157c512385953f0b7ab
Jacob Rogers <intervicker@gmail.com>
2014-10-06 12:54:47 -0500

    fix scoring

--- main/controllers/group.py
+++ main/controllers/group.py
@@ -13,13 +13,13 @@ class SaveGamesApiView(BaseView):
     def post(self, request):
         payload = json.loads(request.POST.dict().keys()[0])
         games = payload['games']
-        scores = sorted([game['score'] for game in games],reverse=True)
+        scores = sorted([int(game['score']) for game in games],reverse=True)
         points = []
         for game in games:
             savedGame = League_Game.objects.get(id=game['id'])
             savedGame.score = game['score']
-            savedGame.league_points = decide_points(scores, savedGame.score)
-            savedGame.bonus_points = decide_bonus_points(scores, savedGame.score)
+            savedGame.league_points = decide_points(scores, int(savedGame.score))
+            savedGame.bonus_points = decide_bonus_points(scores, int(savedGame.score))
             points.append({'id': game['id'], 'league_points': savedGame.league_points, 'bonus_points': savedGame.bonus_points})
             savedGame.save()
         return json_response(points, 201)
