050373ffd590426c0c2873f8f28cc8ed37d4b8d4
Jacob Rogers <intervicker@gmail.com>
2014-10-13 15:15:21 -0500

    fix index for superuser

--- main/controllers/index.py
+++ main/controllers/index.py
@@ -3,7 +3,7 @@ from main.controllers import *
 class IndexView(BaseView):
     
     def doGet(self, request):
-        if (request.user.is_authenticated()):
+        if (request.user.is_authenticated()) and not request.user.is_superuser:
             self.template = 'user_home.html'
             return self.user_home_page(request)
         else:
@@ -26,8 +26,9 @@ class IndexView(BaseView):
                         group['tables'].append(game.table)
                     if game.player not in group['players']:
                         group['players'].append(game.player)
-                print ranking[0]
                 return {'player': player, 'group': group, 'rank': ranking[0].rank}
+            else:
+                return rankings_page
         except Player.DoesNotExist:
             None
         except League_Game.DoesNotExist:
@@ -37,4 +38,4 @@ class IndexView(BaseView):
         weeks = [group.week for group in Group.objects.distinct('week')]
         week = max(weeks) if weeks else 1
         rankings = Ranking.objects.filter(week=week).order_by('rank')
-        return {'week': week, 'weeks': weeks, 'rankings': rankings}
+        return {'week': week, 'rankings': rankings}
