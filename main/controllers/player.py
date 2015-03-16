1c127db6b24fd1e15527ab530ab9e633f9b5cc9f
Jacob Rogers <intervicker@gmail.com>
2015-03-16 12:43:12 -0500

    show winning score on player page

--- main/controllers/player.py
+++ main/controllers/player.py
@@ -14,7 +14,7 @@ class PlayerView(BaseView):
         player_games = []
         for table in sorted({game.table for game in games}, key=attrgetter('name')):
             game = {'table': {'name': table.name, 'id': table.id} }
-            game['scores'] = sorted([{'week': g.group.week, 'score': g.score} for g in games if g.table == table], key=itemgetter('score'), reverse=True)
+            game['scores'] = sorted([{'week': g.group.week, 'score': g.score, 'league_points': g.league_points} for g in games if g.table == table], key=itemgetter('score'), reverse=True)
             player_games.append(game)
 
         week_points = {}
