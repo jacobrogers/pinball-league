3077cc988c66d9f3d6b56b886155eb7d941e1ff2
Jacob Rogers <intervicker@gmail.com>
2015-03-17 22:02:41 -0500

    fix player page

--- main/controllers/index.py
+++ main/controllers/index.py
@@ -12,23 +12,17 @@ class IndexView(BaseView):
 
     def user_home_page(self, request):
         weeks = [group.week for group in Group.objects.distinct('week')]
-
+        
         try:
             currentWeek = max(weeks) if weeks else 1
             player = Player.objects.get(user=request.user)
             group_model = League_Game.objects.filter(player=player, group__week=currentWeek).distinct('group__group')
-            # ranking = player.rankings.filter(week=currentWeek)
             if (group_model):
                 group_model = group_model[0].group
-                group = {'group': group_model.group, 'week': currentWeek, 'tables': [], 'players': []}
-                for game in group_model.games.all():
-                    if game.table not in group['tables']:
-                        group['tables'].append(game.table)
-                    if game.player not in group['players']:
-                        group['players'].append(game.player)
-                return {'player': player, 'group': group, 'rank': '~'}
+                group = {'group': group_model.group, 'week': currentWeek, 'players': group_model.players}
+                return {'player': player, 'group': group}
             else:
-                return rankings_page
+                return self.rankings_page()
         except Player.DoesNotExist:
             None
         except League_Game.DoesNotExist:
